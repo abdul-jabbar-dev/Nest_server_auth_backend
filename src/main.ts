e8aecaa1de091597e501f0e27c8b2ecd71486b33
Abdul Jabbar <abdul.jabbar.dev@gmail.com>
2024-08-02 18:41:35 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { UserModule } from "./user/user.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
-  app.enableCors();
+ app.enableCors({
+        origin: 'https://funny-peony-17f202.netlify.app', 
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+        allowedHeaders: 'Content-Type, Authorization', 
+    });
 
   await app.listen(3000);
 }
