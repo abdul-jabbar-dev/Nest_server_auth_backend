dffd350c9c97d2f89c4e9ac9cf7b96ede6d67d1f
Abdul Jabbar <abdul.jabbar.dev@gmail.com>
2024-08-02 19:10:52 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,20 @@ import * as cors from "cors";
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
 
-  app.use(
-    cors({
-      origin: "*",  
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-      allowedHeaders: "Content-Type, Accept",
-    })
-  );
-
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = ['https://funny-peony-17f202.netlify.app', 'https://funny-peony-17f202.netlify.app/login','https://funny-peony-17f202.netlify.app/registration',origin,'*'];
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Accept",
+  })
+);
   await app.listen(process.env.PORT||3000);
 }
 bootstrap();
